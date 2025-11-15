892bdc3d6746a3bc4314cf843329bd6571e5eeb4
akhilbju <abiju60@gmail.com>
2025-11-15 09:34:49 +0530

    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ]
+})
+export class AppModule {}
